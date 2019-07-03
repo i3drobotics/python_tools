e16de8a92f062d254b8a27995d1dc7ff8c2edc31
bknight-i3drobotics <bknight@i3drobotics.com>
2019-07-03 11:33:13 +0100

    updated stereoBaslerCapture with camera property control

--- stereoBaslerCapture/stereoBaslerCapture.py
+++ stereoBaslerCapture/stereoBaslerCapture.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
 from pypylon import pylon
+import os
 
-maxCamerasToUse = 1
+maxCamerasToUse = 2
 
 # Get the transport layer factory.
 tlFactory = pylon.TlFactory.GetInstance()
@@ -20,9 +21,21 @@ l = cameras.GetSize()
 # Create and attach all Pylon Devices.
 for i, cam in enumerate(cameras):
     cam.Attach(tlFactory.CreateDevice(devices[i]))
+    
 
     # Print the model name of the camera.
     print("Using device ", cam.GetDeviceInfo().GetModelName())
+
+    # Set inital camera properties
+    cam.Open()
+    cam.GetNodeMap().GetNode("ExposureAuto").SetValue("Off")
+    cam.GetNodeMap().GetNode("ExposureTimeRaw").SetValue(40000)
+    cam.GetNodeMap().GetNode("GainAuto").SetValue("Off")
+    cam.GetNodeMap().GetNode("GainRaw").SetValue(0)
+    cam.GetNodeMap().GetNode("PixelFormat").SetValue("Mono8")
+    cam.GetNodeMap().GetNode("Width").SetValue(2448)
+    cam.GetNodeMap().GetNode("Height").SetValue(2048)
+    cam.Close()
 
 cameras.StartGrabbing()
 cv2.namedWindow('stereoBaslerCameras', cv2.WINDOW_NORMAL)
@@ -32,9 +45,10 @@ converter = pylon.ImageFormatConverter()
 converter.OutputPixelFormat = pylon.PixelType_Mono8
 converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
 
+firstImageReceivedR = False
+firstImageReceivedL = False
+
 while cameras.IsGrabbing():
-
-
     grabResult = cameras.RetrieveResult(
         5000, pylon.TimeoutHandling_ThrowException)
 
@@ -46,15 +60,24 @@ while cameras.IsGrabbing():
         image = converter.Convert(grabResult)
         print("Camera ", cameraContextValue, ": ", cameras[cameraContextValue].GetDeviceInfo().GetModelName())
         frame = image.GetArray()
-        #frame_right = image.GetArray()
+        if (not firstImageReceivedR):
+            frame_right = np.zeros(shape=frame.shape,dtype=np.uint8)
+        if (not firstImageReceivedL):
+            frame_left = np.zeros(shape=frame.shape,dtype=np.uint8)
+        if cameraContextValue == 0:
+            frame_left = frame
+            firstImageReceivedL = True
+        elif cameraContextValue == 1:
+            frame_right = frame
+            firstImageReceivedR = True
     
-    #frame_joint = np.concatenate((frame_left,frame_right), axis=1)
-    # Display image
-    cv2.imshow('stereoBaslerCameras', frame)
-    # Close on ESC
-    k = cv2.waitKey(1)
-    if k == 27:
-        break
+        frame_joint = np.concatenate((frame_left,frame_right), axis=1)
+        # Display image
+        cv2.imshow('stereoBaslerCameras', frame_joint)
+        # Close on ESC
+        k = cv2.waitKey(1)
+        if k == 27:
+            break
     
 
 cameras.StopGrabbing()
